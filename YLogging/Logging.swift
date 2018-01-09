0a82fa4475c0ea456ed8167a36a3ee30de71ed00
macbook <macbook@macbooks-MacBook-Pro.local>
2018-01-09 17:37:29 +0530

    Initial commit

---
+++ YLogging/Logging.swift
@@ -0,0 +1,35 @@
+//
+//  Logging.swift
+//  YLogging
+//
+//  Created by macbook on 09/01/18.
+//  Copyright © 2018 macbook. All rights reserved.
+//
+
+import Foundation
+
+public  class YLog {
+    
+    private var isDebug: Bool!
+    
+    //2.
+    public init() {
+        self.isDebug = false
+    }
+    
+    //3.
+    public func setup(isDebug: Bool) {
+        self.isDebug = isDebug
+        print("Project is in Debug mode: \(isDebug)")
+    }
+    
+    //4.
+    public func YPrint<T>(value: T) {
+        if self.isDebug == true {
+            print(value)
+        } else {
+            //Do any stuff for production mode
+        }
+    }
+    
+}
