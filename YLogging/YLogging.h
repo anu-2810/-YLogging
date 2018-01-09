d8b97200fff8b53a829469fa5890ee2d20a1e34f
macbook <macbook@macbooks-MacBook-Pro.local>
2018-01-09 12:53:01 +0530

    Initial Commit

---
+++ YLogging/YLogging.h
@@ -0,0 +1,19 @@
+//
+//  YLogging.h
+//  YLogging
+//
+//  Created by macbook on 09/01/18.
+//  Copyright © 2018 macbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YLogging.
+FOUNDATION_EXPORT double YLoggingVersionNumber;
+
+//! Project version string for YLogging.
+FOUNDATION_EXPORT const unsigned char YLoggingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YLogging/PublicHeader.h>
+
+
